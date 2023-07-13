c4652ae8a79bcac61f4951ab483d667b5a6e5c31
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-13 09:31:54 -0700

    auto pushing

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Program to print a statment
+ * Return: 0 --> int ( Always Success )
+*/
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
