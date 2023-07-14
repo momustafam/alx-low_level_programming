d80e3ccbfd3aa92663d313b5417bfe838c52dc95
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 08:45:26 -0700

    auto pushing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Decription: print all possible combinations of two digits
+ *
+ * Return: 0 --> int (Always Success)
+*/
+
+int main(void)
+{
+
+int i, j;
+
+for (i = 48; i < 57; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
