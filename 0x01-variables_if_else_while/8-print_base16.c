7346bc4d845ddd55de6d13a5cce3c579c8219b2f
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 08:30:30 -0700

    auto pushing

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Decription: print all alphabets in lowercase
+ *
+ * Return: 0 --> int (Always Success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+
+for (i = 97; i < 103: i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
