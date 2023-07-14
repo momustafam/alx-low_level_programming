b9f92aeda81cac513247e1a30143f14fe6b49de6
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 06:51:42 -0700

    auto pushing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Descripttion: generate a random number and print if it is positive, negative or zero
+ *
+ * Return: 0 --> int (Always Success)
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+    printf("%d is positive", n);
+else if (n < 0)
+    printf("%d is negative", n);
+else
+    printf("%d is zero", n);
+
+return (0);
+}
