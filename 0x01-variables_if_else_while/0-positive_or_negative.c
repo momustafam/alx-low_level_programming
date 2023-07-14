f6f5d04706c6903ab16d2289b4e60e35298cdc12
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 06:55:54 -0700

    auto pushing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - entry point
  *
- * Descripttion: generate a random number and print if it is positive, negative or zero
+ * Descripttion: generate a random number and print it
  *
  * Return: 0 --> int (Always Success)
 */
@@ -18,11 +18,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-    printf("%d is positive", n);
+printf("%d is positive", n);
 else if (n < 0)
-    printf("%d is negative", n);
+printf("%d is negative", n);
 else
-    printf("%d is zero", n);
+printf("%d is zero", n);
 
 return (0);
 }
