c9c80b9d2b9e4af59f99caaf17b176823fff08fb
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 07:44:35 -0700

    auto pushing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 
 if (last_digit == 0)
-printf("Last digit of %d is %d and is 0", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 if (last_digit > 5)
-printf("Last digit of %d is %d and is grater than 5 \n", n, last_digit);
+printf("Last digit of %d is %d and is grater than 5\n", n, last_digit);
 else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 return (0);
