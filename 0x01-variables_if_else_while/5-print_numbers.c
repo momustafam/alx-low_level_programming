6b0d9223edcb0420f6c15f0bf4c6c4f4ea0662f5
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 08:24:11 -0700

    auto pushing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Decription: print all digits from 0 to 9
+ *
+ * Return: 0 --> int (Always Success)
+*/
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+
+putchar('\n');
+
+return (0);
+}
