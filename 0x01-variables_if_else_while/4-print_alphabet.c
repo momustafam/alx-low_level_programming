627eea0cfa68b1c73c07f0cbe926c839978c4c0a
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-14 08:16:31 -0700

    auto pushing

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Decription: print all alphabets in lowercase
+ *
+ * Return: 0 --> int (Always Success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+if (i != 'e' || i != 'q')
+{
+putchar(i);
+}
+}
+
+putchar('\n');
+
+return (0);
+}
