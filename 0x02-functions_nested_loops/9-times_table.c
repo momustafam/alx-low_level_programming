0238ac20287a313d215791815631debfc6f78553
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-18 11:41:59 -0700

    auto pushing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,25 +8,29 @@
 
 void times_table(void)
 {
-int i, j;
+int i, j, num;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (j*i < 10)
+num = i * j;
+if (num < 10)
 {
-_putchar(i * j + '0');
+_putchar(num + '0');
 }
 else
 {
-_putchar(i * j / 10 + '0');
-_putchar(i * j % 10 + '0'); 
+_putchar(num / 10 + '0');
+_putchar(num % 10 + '0');
 }
 if (j != 9)
 {
 _putchar(',');
 _putchar(' ');
+if (num < 10)
+{
 _putchar(' ');
+}
 }
 }
 _putchar('\n');
