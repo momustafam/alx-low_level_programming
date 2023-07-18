ec73d10a407e49daa670afa4ca222f03248e2b50
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-18 10:15:07 -0700

    auto pushing

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check if the alphabet is in lowercase or not
+ *
+ * Return: 1 if yes else 0
+ */
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
