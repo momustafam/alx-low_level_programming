068fd5015d9b82258496eb9e1d083f700a2b62ea
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-18 12:30:41 -0700

    auto pushing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,17 +19,23 @@ _putchar(',');
 _putchar(' ');
 prod = num * mult;
 if (prod <= 9)
+{
 _putchar(' ');
 _putchar(' ');
+}
 else if (prod < 100)
+{
 _putchar((prod / 10) + 48);
 _putchar((prod % 10) + 48);
 _putchar(' ');
+}
 else
+{
 _putchar(prod / 100 + 48);
 _putchar((prod / 10) + 48);
 _putchar((prod % 10) + 48);
-} 
+}
+}
 _putchar('\n');
 }
 return;
