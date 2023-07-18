a06d0fe1529fd38eb4fb4ac3787ef43883f82f04
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-18 12:22:25 -0700

    auto pushing

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  times_table - prints the time table of 0 to 9
+ *  @n: starter
+*/
+
+void print_times_table(int n)
+{
+int num, mult, prod;
+if (n > 15 || n < 0)
+return;
+for (num = 0; num <= n; num++)
+{
+_putchar(48);
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + 48);
+_putchar((prod % 10) + 48);
+}
+_putchar('\n');
+}
+return;
+}
