8d18f4c65509aa068fb4525ef4114a0510aaea2f
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-18 12:02:06 -0700

    auto pushing

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - funtion for adding two numbers
+ * @f: the first number
+ * @s: the second number
+ *
+ * Return: f + s
+ */
+
+int add(int f, int s)
+{
+return (f + s);
+}
