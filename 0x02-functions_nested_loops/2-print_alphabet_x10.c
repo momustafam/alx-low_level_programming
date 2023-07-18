392b90f8fd2cd646501fa97516600fd654d75c37
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-18 10:09:49 -0700

    auto pushing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-int i;
+int i, j;
 for (i = 0; i < 10; i++)
 {
-print_alphabet();
-_putchar('\n');
+for (j = 97; j < 123; j++)
+{
+_putchar((char) j);
 }
 _putchar('\n');
+}
 }
