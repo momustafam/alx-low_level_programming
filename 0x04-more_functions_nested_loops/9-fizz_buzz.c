75fde52a141e8d4f2a8264fa41310057e443f0d4
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-20 03:08:32 -0700

    auto pushing

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+void print_fizz(void);
+void print_buzz(void);
+
+/**
+ * Description: main - program that solve the fizz buzz interview question
+ *
+ * Input: Null
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+print_fizz();
+print_buzz();
+}
+else if (i % 3 == 0)
+print_fizz();
+else if (i % 5 == 0)
+print_buzz();
+else
+{
+if (i >= 10)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else
+_putchar(i + '0');
+}
+if (i != 100)
+_putchar(' ');
+}
+_putchar('\n');
+return (0);
+}
+
+
+/**
+ * Desciprtion: print_fizz - write Fizz word using _putchar
+ *
+ * Input: Null
+ *
+ * Return: Null
+ */
+
+void print_fizz(void)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+return;
+}
+
+/**
+ * Desciprtion: print_buzz - write Buzz word using _putchar
+ *
+ * Input: Null
+ *
+ * Return: Null
+ */
+
+
+void print_buzz(void)
+{
+_putchar('B');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+return;
+}
