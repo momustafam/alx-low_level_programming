e1c2e9d215cf9d26add432e5611ced481bd2e250
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-24 06:22:07 -0700

    auto pushing

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Description: _strlen - return the length of a string
+ *
+ * Input:
+ *     @s: [char pointer] --> points to the first character in the string
+ *
+ * Return:
+ *     len [int] --> the length of the string
+ */
+
+int _strlen(char *s)
+{
+        int len=0, i;
+        while (1)
+        {
+            if (*s == '\0')
+                return (len);
+            len++;
+            s += 1;
+        }
+}
