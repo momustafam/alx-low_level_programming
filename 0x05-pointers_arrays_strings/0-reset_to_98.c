f7c32d209fc76e893f778ffa0aba9bd2eb96ad75
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-24 06:01:33 -0700

    auto pushing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * reset_to_98 - that takes a pointer to an int as parameter and 
+ * reset_to_98 - that takes a pointer to an int as parameter and
  *               updates the value it points to to 98
  *
  * Input:
