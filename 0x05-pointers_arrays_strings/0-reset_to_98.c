94e3b112a436165a68dc0a79afb806cac36dc21f
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-24 05:59:35 -0700

    auto pushing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/**
+ * reset_to_98 - that takes a pointer to an int as parameter and 
+ *               updates the value it points to to 98
+ *
+ * Input:
+ *     @n: [int pointer] --> point to an int variable to change its value
+ *
+ * Return: NULL
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+return;
+}
