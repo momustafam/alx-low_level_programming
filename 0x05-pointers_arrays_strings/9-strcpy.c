374bbb38b0d2302d649d88499a43499a35d98f09
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-24 11:51:50 -0700

    auto pushing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,20 +12,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
-
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
+int i = 0;
+do
+{
+dest[i] = src[i];
+i++;
+} while (src[i - 1] != '\0');
+return (dest);
 }
