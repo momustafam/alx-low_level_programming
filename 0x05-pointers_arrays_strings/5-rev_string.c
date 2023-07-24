49a636b89b501a8e8ab0da03e51d0f64af10a579
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-24 08:55:00 -0700

    auto pushing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,21 +11,23 @@
 
 void rev_string(char *s)
 {
-int i, len = 0;
-char temp;
+	char tmp;
+	int i, len, len1;
 
-while (s[len] != '\0')
-{
-len++;
-}
-len--;
-for (i = 0; i < len / 2; i++)
-{
-if (i == len / 2 && len % 2 == 1)
-return;
-temp = s[i];
-s[i] = s[len - i];
-s[len - i] = temp;
-}
-return;
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
