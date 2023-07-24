e5c771114c21d2bad785a93218f16245c37924b3
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-24 09:07:46 -0700

    auto pushing

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * Description: puts2 - prints every other character of a string with the first
+ *
+ * Input:
+ *     @str: the string
+ *
+ * Return: NULL
+ */
+
+void puts2(char *str)
+{
+int i, len = 0;
+while (str[i] != '\0')
+{
+len++;
+}
+len--;
+for (i = 0; i < len; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return;
+}
