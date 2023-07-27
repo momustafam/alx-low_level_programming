efd96ec8eebf5ee1044431747b5506f015e96027
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-27 14:14:53 -0700

    auto pushing

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Description: print_number - print a number
+ * Input:
+ *	@n: [int] --> the number
+ * Return: NULL
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n > 9)
+		print_number(n / 10);
+
+	_putchar(n % 10);
+}
