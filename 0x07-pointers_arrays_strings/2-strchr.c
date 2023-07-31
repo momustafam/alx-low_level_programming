0d22e013a662a4c0a98267d1a09eef021228d66c
Mohammed Mustafa <mohammed112025@gmail.com>
2023-07-31 05:45:07 -0700

    auto pushing

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Description: _strchr - find the first occurrence of a char
+ * Input:
+ *	@s: [char pointer] --> a string for searching
+ *	@c: [char] --> a char what you are looking for
+ * Return: pointer to the first occurrence of the char c
+ *	NULL if the char is not found
+ */
+
+char *_memcpy(char *s, char c)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] == c)
+			return (s + len);
+	}
+	return (NULL);
+}
