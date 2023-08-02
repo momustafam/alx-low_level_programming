60ca9663d79d124586b1583f8a66fc44dce662a6
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-02 09:44:18 -0700

    auto pushing

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: prints a string in reverse using recursion
+ * Input:
+ *	@s: [char pointer] --> points to the string what I wanna print
+ * Return: NULL
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
+
