631461f79eb684cbdbf93bb9f83e2aa4d1fae21c
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-14 12:52:38 -0700

    auto pushing

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,18 +5,30 @@
  * Description: new_dog - creates a new dog
  * Input:
  *	@name: [ponter to char] --> name of the dog
- *	@float: [float] --> the age of the dog
+ *	@age: [float] --> the age of the dog
  *	@owner: [pointer to char] --> name of dog's owner
  * Return: NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	unsigned long int i;
+
 	dog_t *dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 		return ('\0');
-	dog->name = name;
+
+	for (i = 0; i < sizeof(name); i++)
+	{
+		dog->name[i] = name[i];
+	}
+
 	dog->age = age;
-	dog->owner = owner;
+
+	for (i = 0; i < sizeof(owner); i++)
+	{
+		dog->owner[i] = owner[i];
+	}
 
 	return (dog);
 }
