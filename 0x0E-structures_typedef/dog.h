33b2f6c2cde237b0e34f14fee94e501f573a0fad
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-14 11:43:50 -0700

    auto pushing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * Description: struct dog - dog's basic info
+ * Attributes:
+ * @name: [pointer to char] --> name of the dog
+ * @age: [float] --> age of the dog
+ * @owner: [pointer to char] --> name of the dog's owner
+ */
+
+
 struct dog
 {
 	char *name;
