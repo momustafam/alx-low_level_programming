72115b28696616cae80b02730f3b243329220754
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-14 10:56:42 -0700

    auto pushing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: main - prints the name of the compiled file
+ * Input: NULL
+ * Return: 0 Always success
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
