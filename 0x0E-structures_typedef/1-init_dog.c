261a5d1e0f2bfe43184a3aaff3884848c1da72d8
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-14 11:54:54 -0700

    auto pushing

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 /**
  * Description: init_dog - initialize a variable of type struct dog
  * Input:
- *	@d: [struct dog] --> object from the struct dog
+ *	@d: [pointer to struct dog] --> object from the struct dog
  *	@name: [pointer to char] --> name of the dog
  *	@age: [float] --> age of the dog
  *	@owner: [pointer to char] --> the owner of the dog
@@ -12,7 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;	
 }
