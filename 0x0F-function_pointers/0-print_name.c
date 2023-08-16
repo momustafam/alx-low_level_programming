d47ac73118ad9d1ce67af53fc03c2588f15cb4e9
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-16 15:00:32 -0700

    auto pushing

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * Description: print_name - prints a name
+ * Input:
+ *	@name: [pointer to char] --> points to the name what you wanna print
+ *	@f: [pointer to function] --> points to
+ * Return: NULL
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
