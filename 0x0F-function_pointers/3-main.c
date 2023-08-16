5873e38a139a9fd6c4eeb5d605d53c205609b16b
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-16 16:27:57 -0700

    auto pushing

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,41 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
 
-
 /**
- * Description: main - test our program
- * Input:
- *	@argc: [int] --> number of arguments
- *	@argv: [pointer to array] --> array of arguments
- * Return: 0 if success
+ * main - a basic calculator
+ * @argc: the size of the argument vector
+ * @argv: the argument vector
+ *
+ * Description: This program accepts a number, an operator and another number,
+ * in that order. It performs the specified operation and prints the result to
+ * standard output (followed by a newline). If called with the wrong number of
+ * arguments, it prints "Error" and terminates the process with a status of 98.
+ * If the operator given is not one of '+', '-', '*', '/' or '%', it prints
+ * "Error" and terminates the process with a status of 99. If asked to perform
+ * division by 0 (e.g. using the '/' or '%' operators), it prints "Error" and
+ * terminates the process with a status value of 100.
+ *
+ * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int (*f)(int, int);
+
 	if (argc != 4)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(98);
 	}
-	else if (get_op_func(argv[2][0]) == 0)
+
+	f = get_op_func(argv[2]);
+
+	if (!f)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(99);
 	}
-	else if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		print("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(argv[2][0])(atoi(argv[1][0]), atoi(argv[3][0])));
+
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,69 +1,66 @@
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
- * Description: op_add - add two numbers
- * Input:
- *	@a: [int] --> first number
- *	@b: [int] --> second number
- * Return: a + b
+ * op_add - add
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-
-
 /**
- * Description: op_sub - subtract of two elements
- * Input:
- *	@a: [int] --> first number
- *	@b: [int] --> second number
- * Return: a - b
+ * op_sub - subtract
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-
-
 /**
- * Description: op_mul - multiply two integers
- * Input:
- *	@a: [int] --> first number
- *	@b: [int] --> second number
- * Return: a * b
+ * op_mul - multiply
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-
-
 /**
- * Description: op_div - divide two integers
- * Input:
- *	@a: [int] --> first number
- *	@b: [int] --> second number
- * Return: a / b
+ * op_div - divide
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
  */
 int op_div(int a, int b)
 {
+	if (!b)
+	{
+		puts("Error");
+		exit(100);
+	}
 	return (a / b);
 }
 
-
-
 /**
- * Description: op_mod - reminder of divistion of two integers
- * Input:
- *	@a: [int] --> first number
- *	@b: [int] --> second number
- * Return: a % b
+ * op_mod - modulo
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
  */
 int op_mod(int a, int b)
 {
+	if (!b)
+	{
+		puts("Error");
+		exit(100);
+	}
 	return (a % b);
 }
