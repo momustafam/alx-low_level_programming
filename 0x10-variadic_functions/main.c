7f0ed85b9c1d8afe757f595abbf3c266bbd1c600
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-17 05:50:28 -0700

    auto pushing

--- 0x10-variadic_functions/main.c
+++ 0x10-variadic_functions/main.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stddef.h>
 
 /**
  * main - check the code
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-    print_numbers(NULL, 4, 0, 98, -1024, 402);
+    print_all("ceis", 'B', 3, "stSchool");
     return (0);
 }
