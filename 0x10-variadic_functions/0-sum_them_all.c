f52b43a3927924a1dbdf88a7ad9280fcded9763f
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-17 04:42:13 -0700

    auto pushing

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	va_list list;
 	unsigned int i, sum = 0;
-
+	
 	va_start(list, n);
+
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n; i++)
 	{
