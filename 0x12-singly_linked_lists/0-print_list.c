89c3f3c7bcc65e94bfb871701260e5069e2a5d64
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-24 12:34:25 -0700

    auto pushing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		i++;
+
+		nodes++;
 		h = h->next;
 	}
 
-	return (i);
+	return (nodes);
 }
