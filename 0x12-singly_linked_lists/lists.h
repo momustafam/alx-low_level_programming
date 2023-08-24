77ecb9fabd6000128adf8f2189b39d6794b513b9
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-24 12:37:39 -0700

    auto pushing

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,24 @@
 #ifndef LISTS
 #define LISTS
 
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
+*/
 #include <stddef.h>
 #include <stdlib.h>
+
 /**
  * Description: list_t - single linked list
  * @str: [pointer to string] --> a string
  * @len: [unsigned int] -> number of characters in str
  * @next: [pointer to list_t] --> points to next node
  */
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_t *next;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
