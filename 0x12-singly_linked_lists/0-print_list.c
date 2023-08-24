f2f05da4db13af63fcecc8b22f1b1c167f13f343
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-24 12:23:24 -0700

    auto pushing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,10 @@
  *	@h: [pointer to node] --> head of the linked list
  * Return: number of nodes
  */
-size_t print_list(const list_t *h)
+size_t print_list(list_t *h)
 {
-	int i = 1;
-	list_t *temp;
-
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%d] %s\n", h->len, h->str);
-
-	temp = h->next;
+	int i = 0;
+	list_t *temp = h;
 
 	while (1)
 	{
