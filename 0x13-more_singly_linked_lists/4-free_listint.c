2af2bf5360237719c311ade552015816f24e3440
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-29 00:54:48 -0700

    auto pushing

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * Descritpion: free_listint - frees a listint_t list
+ * Input:
+ *	@head: (pointer to listint_t list): the head pointer of the linked list
+ * Return: NULL
+ */
+void free_listint(listint_t *head)
+{
+	/* set temp pointer which points to a node */
+	listint_t *temp = head;
+	/* check if the list is empty exit the program */
+	if (!temp)
+		return;
+	/* loop over the linked list nodes */
+	/* if the ith node is the last node break the loop
+	 * else
+	 *	set the head pointer to next node and remove the current
+	 *	node using temp pointer
+	 */
+	while (temp->next)
+	{
+		head = temp->next;
+		free(temp);
+		temp = head;
+	}
+	/* remove the last node using head */
+	free(temp);
+}
