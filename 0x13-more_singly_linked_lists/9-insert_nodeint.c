a05a2ed1f5342e57d1424f8dfeb1c487198c123d
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-29 04:06:35 -0700

    auto pushing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0;
 	listint_t *new = malloc(sizeof(listint_t));
 
-	if (!new)
+	if (!new || !head)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
