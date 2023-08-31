fa653c76799baec4ca569940f40590a094fbdfa5
Mohammed Mustafa <mohammed112025@gmail.com>
2023-08-31 12:46:40 -0700

    auto pushing

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,78 +1,23 @@
 #include "main.h"
 
-void first_one(unsigned long int *, unsigned long int *);
-
 /**
- * Decription: print_binary - prints the binary representation of a number
- * Input:
- *	@n: (unsigned long int): a number in base 10
- * Return: NULL
- */
+ * print_binary - prints the binary representation of a number.
+ * @n: number
+*/
 void print_binary(unsigned long int n)
 {
-	unsigned long int len = 0, j = 0, i = 0;
-	char *ans;
+	int size = sizeof(n) * 8, flag = 0;
 
-	if (n == 0)
+	while (size)
 	{
-		printf("0");
-		return;
-	}
-
-	first_one(&n, &i);
-	len = i;
-	ans = malloc(sizeof(char) * len);
-
-	if (ans == NULL)
-		return;
-
-	for (j = 0; j < i; j++)
-	{
-		ans[j] = '0';
-	}
-
-	ans[0] = '1';
-
-	while (n > 0)
-	{
-		first_one(&n, &i);
-		ans[len - i] = '1';
-	}
-	printf("%s", ans);
-	free(ans);
-}
-
-
-
-/**
- * Description: first_one - returns index of first one in binary representation
- * of an integer
- * Input:
- *	@n: (unsigned long int): a number in base 10
- *	@i: (unsigned long int): index
- * Return: NULL
- */
-void first_one(unsigned long int *n, unsigned long int *i)
-{
-	unsigned long int multi_two = 1;
-	*i = 1;
-
-	while (1)
-	{
-		if (multi_two == *n)
+		if (n & 1L << --size)
 		{
-			*n = 0;
-			return;
+			_putchar('1');
+			flag++;
 		}
-		else if (multi_two > *n)
-		{
-			*i -= 1;
-			*n -= multi_two / 2;
-			return;
-		}
-
-		multi_two *= 2;
-		*i += 1;
+		else if (flag)
+			_putchar('0');
 	}
+	if (!flag)
+		_putchar('0');
 }
-
