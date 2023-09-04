41e4471085f37089910c162de41522256d734d7f
Mohammed Mustafa <mohammed112025@gmail.com>
2023-09-04 07:50:29 -0700

    auto pushing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,8 +24,12 @@ int create_file(const char *filename, char *text_content)
 		fp = open(filename, O_CREAT, 0600);
 
 	fp = write(fp, text_content, sizeof(text_content));
-
+	
 	if (fp < 0)
+	{
+		close(fp);
 		return (-1);
+	}
+	close(fp);
 	return (1);
 }
