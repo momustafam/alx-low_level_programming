21145ccff4c787fdf73fa8c91aa7cb01a2b6547b
Mohammed Mustafa <mohammed112025@gmail.com>
2023-09-04 07:58:01 -0700

    auto pushing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,22 +11,34 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fp_open, fp_write;
+	int fd_open, fd_write;
 
 	if (filename == NULL)
 		return (-1);
-
-	fp_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (fp_open < 0)
+	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fd_open == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		fp_write = write(fp_open, text_content, sizeof(text_content));
-		if (fp_write < 0)
+		fd_write = write(fd_open, text_content, _strlen(text_content));
+		if (fd_write == -1)
 			return (-1);
 	}
-
-	close(fp_open);
+	close(fd_open);
 	return (1);
+}
+
+/**
+ * _strlen - calculate the lenght of a string.
+ * @str: array of characters.
+ *
+ * Return: lenght of the string.
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str++)
+		count++;
+	return (count);
 }
