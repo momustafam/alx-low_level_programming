b4ae7464e303c53cce7837295cfab29e691c41ac
Mohammed Mustafa <mohammed112025@gmail.com>
2023-09-04 06:14:14 -0700

    auto pushing

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * Description: read_textfile - reads a text file and prints it to the
+ * POSIX standard output.
+ * Input:
+ *	@filename: (pointer to const char): pointer to a file
+ *	@letters: (size_t): number of characters it should read and print
+ * Return:
+ *	-the actual number of characters it could read and print
+ *	- 0 if the file can not be opened or read, the filename is NULL, or
+ *	write fails or does not write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp, sz;
+	char *buffer = calloc(letters + 10, sizeof(char));
+
+	fp = open(filename, O_RDONLY);
+	sz = read(fp, buffer, letters);
+
+	if (sz < 0 || close(fp) < 0)
+		return (sz);
+
+	printf("%s\n", buffer);
+		return (sz);
+}
