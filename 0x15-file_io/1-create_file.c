d7934b196da44f40e615683b79f2204a7ec7ecc2
Mohammed Mustafa <mohammed112025@gmail.com>
2023-09-04 07:42:22 -0700

    auto pushing

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * Description: create_file - function that creates a file
+ * Input:
+ *	@filename: (pointer to const char): name of the file
+ *	@text_content: (pointer to char): points to the content of the file
+ * Return:
+ *	- 1 if success
+ *	- -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = '\0';
+	
+	fp = open(filename, O_CREAT | O_WRONLY, 0600);
+	
+	if (fp < 0)
+		return (-1);
+
+	fp = write(fp, text_content, sizeof(text_content));
+
+	if (fp < 0)
+		return (-1);
+	return (1);
+}
