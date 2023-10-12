a3a2689f87020ce84ad09433c8fb1272ed3e9466
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-12 12:18:32 -0700

    Auto pushing

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ *
+ * Parameters:
+ * -----------
+ * @h: head pointer of the doubly linked list
+ * @idx: index of the new node
+ * @n: data value of the new node
+ *
+ * Return:
+ * -------
+ *  - the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *prev, *cur, *new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new || !h)
+		return (NULL);
+
+	prev = NULL;
+	cur = *h;
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = cur;
+		*h = new;
+		return (new);
+	}
+
+	while (cur)
+	{
+		if (i == idx)
+		{
+			prev->next = new;
+			cur->prev = new;
+			new->next = cur;
+			new->prev = prev;
+			return (new);
+		}
+		prev = cur;
+		cur = cur->next;
+		i++;
+	}
+	return (NULL);
+}
