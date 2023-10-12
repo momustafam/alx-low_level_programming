cde801b5aa43b100ed7f12443700f310b582fbf6
Mohammed Mustafa <mohammed112025@gmail.com>
2023-10-12 06:01:42 -0700

    Auto pushing

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * Description: size_dlistint_len - returns number of elements of a dlistint_t
+ * Description: dlistint_len - returns number of elements of a dlistint_t list
  *
  * Parameters:
  * -----------
@@ -12,7 +12,7 @@
  * --------
  * the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int size = 0;
 
