8dd34cfcac4f76fd9670d78c1fcc79ced3d89d5e
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-03 14:06:36 -0700

    auto pushing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int index;
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
 	if (!hash_table)
 		return (NULL);
 
 	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!(hash_table->array))
 	{
 		free(hash_table);
 		return (NULL);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		hash_table->array[index] = NULL;
 	}
 
 	return (hash_table);
