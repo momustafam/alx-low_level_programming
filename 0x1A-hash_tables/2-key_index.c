a203cb074570b4d6c19efe5f4c9a89a744683fcd
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-03 15:28:29 -0700

    auto pushing

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,5 +13,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index = hash_djb2(key);
 	(void)size;
 
-	return (index%1024);
+	return (index % 1024);
 }
