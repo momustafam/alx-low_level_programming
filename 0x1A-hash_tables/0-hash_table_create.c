64a79dbef6f09a072bf7019cd9117e1fe9f8b040
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-03 14:30:45 -0700

    auto pushing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,23 +1,34 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_create - creates a hash table
- *
- * @size: the size of the array of the hash table
+ * hash_table_create -  creates a hash table.
+ * @size: size of the array
  *
  * Return: a pointer to the newly created hash table
- */
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	hash_table_t *table;
+	unsigned long int i;
 
-	if (!hash_table)
+	/* allocate memory for hash table */
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
-
-	if (!(hash_table->array))
+	table->size = size;
+	/* allocate memory for the array */
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
-	return (hash_table);
+	/* initialize array */
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
