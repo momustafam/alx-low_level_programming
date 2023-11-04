47a68358f63cd31ff4e3821da3875b2dc4bbc9c2
Mohammed Mustafa <mohammed112025@gmail.com>
2023-11-03 18:27:54 -0700

    auto pushing

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,29 +1,31 @@
 #include "hash_tables.h"
 
 /**
- *
  * hash_table_delete - deletes a hash table.
- * @ht: the hash table to delete
+ *
+ * @ht: the hash table what you want to delete
+ *
+ * Return: NULL
 */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *next;
+	hash_node_t *temp, *head;
 	unsigned long i = 0;
 
-	while (i < ht->size)
+	for (i = 0; i < ht->size; i++)
 	{
-		while (ht->array[i])
+		head = ht->array[i];
+		ht->array[i] = NULL;
+
+		while (head)
 		{
-			next = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = next;
+			temp = head;
+			head = head->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
 		}
-		i++;
 	}
 	free(ht->array);
-	ht->array = NULL;
-	ht->size = 0;
 	free(ht);
 }
